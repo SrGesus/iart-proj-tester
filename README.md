03fa65c9318cdb7acf5f03b4176a341cdf97388f
SrGesus <108523575+SrGesus@users.noreply.github.com>
2024-05-05 11:13:19 +0100

    Finished test framework

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # iart-proj-tester
 Unit tests for artificial intelligence project
+
+## Instalation
+Clone the repository and install pytest
+```bash
+git clone https://github.com/SrGesus/iart-proj-tester
+cd iart-proj-tester
+pip install -r requirements.txt
+```
+Configure the variables to match your setup
+```python
+# Test will abort after timeout seconds
+timeout = 10
+# Path to tests folder
+tests_path = "./tests/"
+# Path to python script
+python_path = "./pipe.py"
+```
+
+## Running
+Use pytests to run the tests, e.g:
+```bash
+python -m pytest tests.py 
+```
---
+++ test_pipe.py
@@ -0,0 +1,34 @@
+from subprocess import check_output, TimeoutExpired
+import os
+import pytest
+
+## Configuration
+# Test will abort after timeout seconds
+timeout = 10
+# Path to tests folder
+tests_path = "./tests/"
+# Path to python script
+python_path = "../IArt-PipesMania/pipe.py"
+
+## Imports python script as module named pipe
+## Can be used for more precise unit tests
+# import importlib.util
+# import sys
+# module_name = os.path.basename(python_path)[:-3]
+# spec = importlib.util.spec_from_file_location(module_name, python_path)
+# pipe = importlib.util.module_from_spec(spec)
+# sys.modules["module.name"] = pipe
+# spec.loader.exec_module(pipe)
+
+@pytest.mark.parametrize('file_name', [tests_path + "/" + s for s in os.listdir(tests_path) if s.endswith(".txt")])
+def test_outputs(file_name):
+  file = open(file_name)
+  file_out = open(file_name.replace(".txt", ".out"))
+  try:
+    p = check_output(['python3', 'pipe.py'], stdin=file, timeout=timeout)
+    assert file_out.read() == p # Test
+  except TimeoutExpired:
+    pytest.fail(f'Timed limit exceeded after {timeout} seconds')
+
+
+  
